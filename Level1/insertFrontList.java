68d2df39412755de06eb98c72d583ea47c4b2279
RKCP <rkcpeters@gmail.com>
2020-06-12 17:01:11 +0100

    New solution for insertAtHead.

--- Level1/insertFrontList.java
+++ Level1/insertFrontList.java
@@ -9,18 +9,11 @@ public class insertFrontList {
 
     public ListNode insertAtHead(ListNode head, int data) {
 
-        ListNode newHead = new ListNode(data);
+        ListNode d = new ListNode(data);
 
-        if (head == null) {
-            return newHead;
-        } else {
-            newHead.next = head;
-        }
+        d.next = head;
 
-        return newHead;
-
-
-
+        return d;
 
     }
 
